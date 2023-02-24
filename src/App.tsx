12d54e940b42b56a8b1998db08699c8d6fd2ce41
Andrii Koptiev <koptev.andrey87@gmail.com>
2023-02-24 13:52:34 +0200

    added drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import React from 'react';
 import './App.css';
+import { MiniDrawer } from './components/Drawer';
 
 function App() {
-    return <div className="App">Developing</div>;
+    return (
+        <div className="App">
+            <MiniDrawer />
+        </div>
+    );
 }
 
 export default App;
