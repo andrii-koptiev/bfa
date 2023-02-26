0c9b01a6b84f351dfac37d50ac16170d26bc398d
Andrii Koptiev <koptev.andrey87@gmail.com>
2023-02-26 19:47:29 +0200

    added login logout

---
+++ src/utils/googleAuthConfig.ts
@@ -0,0 +1,19 @@
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBql3Qdxv_9LeHVlcbpz3AsmL0lO6oDZrE',
+    authDomain: 'buyer-financial-accounting.firebaseapp.com',
+    databaseURL:
+        'https://buyer-financial-accounting-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'buyer-financial-accounting',
+    storageBucket: 'buyer-financial-accounting.appspot.com',
+    messagingSenderId: '260850141161',
+    appId: '1:260850141161:web:d491dc68cdc15edc8aed78',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const provider = new GoogleAuthProvider();
